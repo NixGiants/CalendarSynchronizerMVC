7a5e67bdff9a6cbcc244d4821ec50620afad00b0
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-20 13:34:47 +0200

    Create AccountController

---
+++ CalenadrSynchronizerMVC/CalenadrSynchronizerMVC/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalenadrSynchronizerMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register(string? returnUrl = null)
+        {
+
+        }
+    }
+}
