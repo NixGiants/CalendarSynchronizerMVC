6f5b566b633188c6f7ebc5437c1e8e9a1e8ea9e8
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-20 14:12:04 +0200

    Write ControllerMethod

--- CalenadrSynchronizerMVC/CalenadrSynchronizerMVC/Controllers/AccountController.cs
+++ CalenadrSynchronizerMVC/CalenadrSynchronizerMVC/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using CalenadrSynchronizerMVC.Models;
+using CalenadrSynchronizerMVC.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CalenadrSynchronizerMVC.Controllers
@@ -21,7 +23,34 @@ namespace CalenadrSynchronizerMVC.Controllers
 
         public async Task<IActionResult> Register(string? returnUrl = null)
         {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            registerViewModel.ReturnUrl = returnUrl;
+            return View(registerViewModel);
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel, string? returnUrl)
+        {
+            registerViewModel.ReturnUrl = returnUrl;
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser
+                {
+                    UserName = registerViewModel.UserName,
+                    Email = registerViewModel.Email,
+                };
+                var result = await userManager.CreateAsync(user, registerViewModel.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return LocalRedirect(returnUrl);
+                }
+                ModelState.AddModelError("Password", "User couldm't be created. Password not good enough");
+
+            }
+            return View(registerViewModel);
         }
     }
 }
