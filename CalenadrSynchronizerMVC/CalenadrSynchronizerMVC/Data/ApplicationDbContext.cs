f06df75b97532e0f75a33e76b60ae7812de2f790
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-20 13:03:04 +0200

    Create DbContext class

---
+++ CalenadrSynchronizerMVC/CalenadrSynchronizerMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CalenadrSynchronizerMVC.Data
+{
+    public class ApplicationDbContext:IdentityDbContext<IdentityUser>
+    {
+    }
+}
