8e3b5ef8349e055d92ca15c435569311ad128710
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-11-20 14:28:30 +0200

    Migration To Propper Database

--- CalenadrSynchronizerMVC/CalenadrSynchronizerMVC/Migrations/20221120122704_UserMigration2.Designer.cs
+++ CalenadrSynchronizerMVC/CalenadrSynchronizerMVC/Migrations/20221120122704_UserMigration2.Designer.cs
@@ -4,6 +4,7 @@ using CalenadrSynchronizerMVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CalenadrSynchronizerMVC.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221120122704_UserMigration2")]
+    partial class UserMigration2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CalenadrSynchronizerMVC/CalenadrSynchronizerMVC/Migrations/20221120122704_UserMigration2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CalenadrSynchronizerMVC.Migrations
+{
+    public partial class UserMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
